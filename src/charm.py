2a234a915a3049527fa0c5b82ad1380895c2a609
Raul Zamora <raul.zamora@canonical.com>
2022-01-31 09:33:29 +0100

    initial commit

---
+++ src/charm.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# Copyright 2022 Raúl Zamora
+# See LICENSE file for licensing details.
+#
+# Learn more at: https://juju.is/docs/sdk
+
+"""Charm the service.
+
+Refer to the following post for a quick-start guide that will help you
+develop a new k8s charm using the Operator Framework:
+
+    https://discourse.charmhub.io/t/4208
+"""
+
+import logging
+import urllib
+
+from charms.nginx_ingress_integrator.v0.ingress import IngressRequires
+from ops.charm import CharmBase
+from ops.main import main
+from ops.model import ActiveStatus, WaitingStatus, MaintenanceStatus
+
+
+logger = logging.getLogger(__name__)
+
+
+class MydemoCharm(CharmBase):
+    """Charm the service."""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.install, self._on_install)
+        self.framework.observe(self.on.pull_site_action, self._pull_site_action)
+
+        self.ingress = IngressRequires(self, {
+            "service-hostname": "mydemo.juju",
+            "service-name": self.app.name,  # self.unit.name
+            "service-port": 8080
+        })
+
+    def _on_install(self, _):
+        # Download the site
+        self._fetch_site()
+
+    def _on_config_changed(self, event):
+        """Handle the config-changed event"""
+        # Get the gosherve container so we can configure/manipulate it
+        container = self.unit.get_container("gosherve")
+
+        # Create a new config layer
+        # redirect = self.config['redirect-map']
+        layer = self._gosherve_layer()
+
+        if container.can_connect():
+            # Get the current config
+            services = container.get_plan().to_dict().get("services", {})
+            # Check if there are any changes to services
+            if services != layer["services"]:
+                # Changes were made, add the new layer
+                container.add_layer("gosherve", layer, combine=True)
+                logging.info("Added updated layer 'gosherve' to Pebble plan")
+                # Restart it and report a new status to Juju
+                container.restart("gosherve")
+                logging.info("Restarted gosherve service")
+            # All is well, set an ActiveStatus
+            self.unit.status = ActiveStatus()
+        else:
+            self.unit.status = WaitingStatus("waiting for Pebble in workload container")
+
+    def _gosherve_layer(self):
+        """Returns a Pebble configration layer for Gosherve"""
+        return {
+            "summary": "gosherve layer",
+            "description": "pebble config layer for gosherve",
+            "services": {
+                "gosherve": {
+                    "override": "replace",
+                    "summary": "gosherve",
+                    "command": "/gosherve",
+                    "startup": "enabled",
+                    "environment": {
+                        "REDIRECT_MAP_URL": self.config['redirect-map'],
+                        "WEBROOT": "/srv"
+                    },
+                }
+            },
+        }
+
+    def _fetch_site(self):
+        """Fetch latest copy of website from Github and move into webroot"""
+        # Set the site URL
+        site_src = "https://jnsgr.uk/demo-site"
+        # Set some status and do some logging
+        self.unit.status = MaintenanceStatus("Fetching web site")
+        logger.info("Downloading site from %s", site_src)
+        # Download the site
+        urllib.request.urlretrieve(site_src, "/srv/index.html")
+        # Set the unit status back to Active
+        self.unit.status = ActiveStatus()
+
+    def _pull_site_action(self, event):
+        """Handler for the pull_site_action"""
+        event.log('This is an event log message')
+        event.log('Calling site')
+        self._fetch_site()
+        event.log('Site pulled')
+        event.set_results({
+            "result": "Site pulled",
+            "key": "some value"
+        })
+
+
+if __name__ == "__main__":
+    main(MydemoCharm)
---
+++ tests/test_charm.py
@@ -0,0 +1,97 @@
+# Copyright 2022 Raúl Zamora
+# See LICENSE file for licensing details.
+#
+# Learn more about testing at: https://juju.is/docs/sdk/testing
+
+import unittest
+from unittest.mock import patch
+
+from charm import MydemoCharm
+from ops.model import ActiveStatus
+from ops.testing import Harness
+
+
+class TestCharm(unittest.TestCase):
+    def setUp(self):
+        self.harness = Harness(MydemoCharm)
+        self.addCleanup(self.harness.cleanup)
+        self.harness.begin()
+
+    def test_gosherve_layer(self):
+        # Test with empty config.
+        self.assertEqual(self.harness.charm.config["redirect-map"], "https://jnsgr.uk/demo-routes")
+        expected = {
+            "summary": "gosherve layer",
+            "description": "pebble config layer for gosherve",
+            "services": {
+                "gosherve": {
+                    "override": "replace",
+                    "summary": "gosherve",
+                    "command": "/gosherve",
+                    "startup": "enabled",
+                    "environment": {
+                        "REDIRECT_MAP_URL": "https://jnsgr.uk/demo-routes",
+                        "WEBROOT": "/srv",
+                    },
+                }
+            },
+        }
+        self.assertEqual(self.harness.charm._gosherve_layer(), expected)
+        # And now test with a different value in the redirect-map config option.
+        # Disable hook firing first.
+        self.harness.disable_hooks()
+        self.harness.update_config({"redirect-map": "test value"})
+        expected["services"]["gosherve"]["environment"]["REDIRECT_MAP_URL"] = "test value"
+        self.assertEqual(self.harness.charm._gosherve_layer(), expected)
+
+    def test_on_config_changed(self):
+        plan = self.harness.get_container_pebble_plan("gosherve")
+        self.assertEqual(plan.to_dict(), {})
+        # Trigger a config-changed hook. Since there was no plan initially, the
+        # "gosherve" service in the container won't be running so we'll be
+        # testing the `is_running() == False` codepath.
+        self.harness.update_config({"redirect-map": "test value"})
+        plan = self.harness.get_container_pebble_plan("gosherve")
+        # Get the expected layer from the gosherve_layer method (tested above)
+        expected = self.harness.charm._gosherve_layer()
+        expected.pop("summary", "")
+        expected.pop("description", "")
+        # Check the plan is as expected
+        self.assertEqual(plan.to_dict(), expected)
+        self.assertEqual(self.harness.model.unit.status, ActiveStatus())
+        container = self.harness.model.unit.get_container("gosherve")
+        self.assertEqual(container.get_service("gosherve").is_running(), True)
+
+        # Now test again with different config, knowing that the "gosherve"
+        # service is running (because we've just tested it above), so we'll
+        # be testing the `is_running() == True` codepath.
+        self.harness.update_config({"redirect-map": "test2 value"})
+        plan = self.harness.get_container_pebble_plan("gosherve")
+        # Adjust the expected plan
+        expected["services"]["gosherve"]["environment"]["REDIRECT_MAP_URL"] = "test2 value"
+        self.assertEqual(plan.to_dict(), expected)
+        self.assertEqual(container.get_service("gosherve").is_running(), True)
+        self.assertEqual(self.harness.model.unit.status, ActiveStatus())
+
+        # And finally test again with the same config to ensure we exercise
+        # the case where the plan we've created matches the active one. We're
+        # going to mock the container.stop and container.start calls to confirm
+        # they were not called.
+        with patch('ops.model.Container.start') as\
+                _start, patch('ops.model.Container.stop') as\
+                _stop:
+            self.harness.charm.on.config_changed.emit()
+            _start.assert_not_called()
+            _stop.assert_not_called()
+
+    # @patch("charm.HelloKubeconCharm._fetch_site")
+    # def test_on_install(self, _fetch_site):
+    #     self.harness.charm._on_install("mock_event")
+    #     _fetch_site.assert_called_once
+
+    # @patch("charm.HelloKubeconCharm._fetch_site")
+    # def test_pull_site_action(self, _fetch_site):
+    #     mock_event = Mock()
+    #     self.harness.charm._pull_site_action(mock_event)
+    #     _fetch_site.assert_called_once
+    #     mock_event.called_once_with({"result": "site pulled"})
